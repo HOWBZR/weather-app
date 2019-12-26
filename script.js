e433b3594d7966869b15a9e7e3a288a0c77d8a2b
David Coons <dlc9005@gmail.com>
2019-12-26 13:41:13 -0700

    added cards with weather info and graphics. Still need to save data to local storage

--- script.js
+++ script.js
@@ -11,19 +11,20 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
             let temperature = (response.main.temp - 273) * 1.8 + 32
             let temp = temperature.toFixed(2)
             let lat = response.coord.lat
             let lon = response.coord.lon
             let countryCode = response.sys.country
+            var today = new Date();
+            var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
             
-
+            console.log(response)
             //populating info for current city info
             $('#temp').text('Temperature: ' + temp + ' degrees');
             $('#humidity').text("Humidity: " + response.main.humidity + '%')
-            $('#wind').text('Wind Speed ' + response.wind.speed + ' mph')
-            $('#city-name').text(searchterm.value)
+            $('#wind').text('Wind Speed: ' + response.wind.speed + ' mph')
+            $('#city-name').text(searchterm.value.toUpperCase() + ' ' + date )
 
             //UV Index Function for current city UV info
             const uvURL = 'http://api.openweathermap.org/data/2.5/uvi?appid=79b9010c856142d3dabc51dccb05cdb8&lat=' + lat + '&lon=' + lon
@@ -34,19 +35,22 @@ $(document).ready(function () {
                 $('#uv').text('UV Index: ' + response.value)
             })
 
-
+            // start 5b day forecast population
             const fivedayURL = 'http://api.openweathermap.org/data/2.5/forecast?appid=79b9010c856142d3dabc51dccb05cdb8&q=' + city + ',' + countryCode
             
             $.ajax({
                 url: fivedayURL,
                 method: 'GET'
             }).then(function (response) {
-                
-                
+               
                 for (let i = 0; i < 6; i++){
-                    console.log(response.list[i].main.temp)
-                    
-                    $('#day'+[i]).html(response.list[i].main.temp)
+                    let temps = (response.list[i].main.temp - 273) * 1.8 + 32
+                    let ftemps = temps.toFixed(2)
+                    var dateF = (today.getMonth() + 1) + '/' + (today.getDate() + i) + '/' + today.getFullYear();
+                    $('#date' + [i]).html(dateF)               
+                    $('#icon' + [i]).html('<img src=http://openweathermap.org/img/wn/' + response.list[i].weather[0].icon +'@2x.png>')
+                    $('#day' + [i]).html("Temp: " + ftemps)
+                    $('#humidity' + [i]).html("Humidity: " + response.list[i].main.humidity)
                    
                 }
             })
